0b9c5e2b5d0a2ad692aaa20fdfb034e55f119fd1
Ari Pollak <aripollak@gmail.com>
2010-08-11 01:14:57 -0400

    Imported Upstream version 2.2.8

--- plug-ins/common/newsprint.c
+++ plug-ins/common/newsprint.c
@@ -59,7 +59,7 @@
 
 
 #ifdef RCSID
-static char rcsid[] = "$Id: newsprint.c,v 1.69.2.1 2005/04/18 19:07:24 yosh Exp $";
+static char rcsid[] = "$Id: newsprint.c,v 1.69.2.2 2005/06/21 22:11:36 weskaggs Exp $";
 #endif
 
 #define VERSION "v0.60"
@@ -1863,16 +1863,12 @@ do {                                                            \
       spotfn_list[rf].thresh = spot2thresh (rf, width);
       thresh[0] = spotfn_list[rf].thresh;
 
-      /* optimisation: only use separate threshold matrices if the
-       * spot functions are actually different */
       ASRT (gf);
-      if (!spotfn_list[gf].thresh)
-        spotfn_list[gf].thresh = spot2thresh (gf, width);
+      spotfn_list[gf].thresh = spot2thresh (gf, width);
       thresh[1] = spotfn_list[gf].thresh;
 
       ASRT (bf);
-      if (!spotfn_list[bf].thresh)
-        spotfn_list[bf].thresh = spot2thresh (bf, width);
+      spotfn_list[bf].thresh = spot2thresh (bf, width);
       thresh[2] = spotfn_list[bf].thresh;
 
       if (colourspace == CS_CMYK)
@@ -1880,8 +1876,7 @@ do {                                                            \
           rot[3] = DEG2RAD (pvals.gry_ang);
           gf = pvals.gry_spotfn;
           ASRT (gf);
-          if (!spotfn_list[gf].thresh)
-            spotfn_list[gf].thresh = spot2thresh (gf, width);
+	  spotfn_list[gf].thresh = spot2thresh (gf, width);
           thresh[3] = spotfn_list[gf].thresh;
         }
     }
